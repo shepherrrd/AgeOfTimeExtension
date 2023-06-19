36c1fe2981564678a771b80233768416bbe56369
shepherrrd <shefkid2@gmail.com>
2023-06-19 17:45:45 +0100

    Rounding Up Problem Solved And Official Release

--- src/AgeofTime.cs
+++ src/AgeofTime.cs
@@ -5,41 +5,41 @@ public static class AgeOfTime
     public static string AgeofTime(this DateTimeOffset date)
     {
         DateTime now = DateTime.Now;
-        TimeSpan timeSincePost = ( date - now ) ;
-      
-        if (timeSincePost.TotalDays >= 365)
+        TimeSpan timeSincePost = date - now ;
+       // return Math.Round(timeSincePost.TotalDays).ToString();
+        if (Math.Round(timeSincePost.TotalDays) >= 365)
         {
-            int years = (int)(timeSincePost.TotalDays / 365);
+            int years = (int) Math.Round(timeSincePost.TotalDays / 365);
             return $"{years} year{(years > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalDays >= 30)
+        else if (Math.Round(timeSincePost.TotalDays) >= 30)
         {
-            int months = (int)(timeSincePost.TotalDays / 30);
+            int months = (int)Math.Round((timeSincePost.TotalDays / 30));
             return $"{months} month{(months > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalDays >= 7)
+        else if (Math.Round(timeSincePost.TotalDays) >= 7)
         {
-            int weeks = (int)(timeSincePost.TotalDays / 7);
+            int weeks = (int)Math.Round((timeSincePost.TotalDays / 7));
             return $"{weeks} week{(weeks > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalDays >= 1)
+        else if (Math.Round(timeSincePost.TotalDays) >= 1)
         {
-            int days = (int)timeSincePost.TotalDays;
+            int days = (int)Math.Round(timeSincePost.TotalDays);
             return $"{days} day{(days > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalHours >= 1)
+        else if (Math.Round(timeSincePost.TotalHours) >= 1)
         {
-            int hours = (int)timeSincePost.TotalHours;
+            int hours = (int) Math.Round(timeSincePost.TotalHours);
             return $"{hours} hour{(hours > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalMinutes >= 1)
+        else if (Math.Round(timeSincePost.TotalMinutes) >= 1)
         {
-            int minutes = (int)timeSincePost.TotalMinutes;
+            int minutes = (int)Math.Round(timeSincePost.TotalMinutes);
             return $"{minutes} minute{(minutes > 1 ? "s" : "")} ago";
         }
         else
         {
-            int seconds = (int)timeSincePost.TotalSeconds;
+            int seconds = (int)Math.Round(timeSincePost.TotalSeconds);
             return $"{seconds} second{(seconds > 1 ? "s" : "")} ago";
         }
 
@@ -47,41 +47,42 @@ public static class AgeOfTime
     public static string AgeofTime(this DateTime date)
     {
         DateTime now = DateTime.Now;
-   TimeSpan timeSincePost = ( date - now ) ;
 
-        if (timeSincePost.TotalDays >= 365)
+        TimeSpan timeSincePost = date - now  ;
+
+        if (Math.Round(timeSincePost.TotalDays) >= 365)
         {
-            int years = (int)(timeSincePost.TotalDays / 365);
+            int years = (int)Math.Round(timeSincePost.TotalDays / 365);
             return $"{years} year{(years > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalDays >= 30)
+        else if (Math.Round(timeSincePost.TotalDays) >= 30)
         {
-            int months = (int)(timeSincePost.TotalDays / 30);
+            int months = (int)(Math.Round(timeSincePost.TotalDays) / 30);
             return $"{months} month{(months > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalDays >= 7)
+        else if (Math.Round(timeSincePost.TotalDays) >= 7)
         {
-            int weeks = (int)(timeSincePost.TotalDays / 7);
+            int weeks = (int)Math.Round(timeSincePost.TotalDays / 7);
             return $"{weeks} week{(weeks > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalDays >= 1)
+        else if (Math.Round(timeSincePost.TotalDays)  >= 1)
         {
-            int days = (int)timeSincePost.TotalDays;
+            int days = (int)Math.Round(timeSincePost.TotalDays);
             return $"{days} day{(days > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalHours >= 1)
+        else if (Math.Round(timeSincePost.TotalHours) >= 1)
         {
-            int hours = (int)timeSincePost.TotalHours;
+            int hours = (int)Math.Round(timeSincePost.TotalHours);
             return $"{hours} hour{(hours > 1 ? "s" : "")} ago";
         }
-        else if (timeSincePost.TotalMinutes >= 1)
+        else if (Math.Round(timeSincePost.TotalMinutes) >= 1)
         {
-            int minutes = (int)timeSincePost.TotalMinutes;
+            int minutes = (int)Math.Round(timeSincePost.TotalMinutes);
             return $"{minutes} minute{(minutes > 1 ? "s" : "")} ago";
         }
         else
         {
-            int seconds = (int)timeSincePost.TotalSeconds;
+            int seconds = (int)Math.Round(timeSincePost.TotalSeconds);
             return $"{seconds} second{(seconds > 1 ? "s" : "")} ago";
         }
     }
